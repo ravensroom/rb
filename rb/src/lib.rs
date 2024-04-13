cd1fab4836e0c52eeb016f314dd039c580378acb
raven <raven.lin.010@gmail.com>
2024-04-13 13:03:26 -0700

    tb-16: threads, channel and mutex

--- rb/src/lib.rs
+++ rb/src/lib.rs
@@ -5,5 +5,6 @@ pub mod _06;
 pub mod _08;
 pub mod _12;
 pub mod _13;
+pub mod _16;
 
 pub use _12::minigrep::run as minigrep;
--- rb/src/main.rs
+++ rb/src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
-    use cp::_2::number_converter;
-    number_converter::from_bin();
+    use rb::_16::fearless_concurrency;
+    fearless_concurrency::mutex();
 }
